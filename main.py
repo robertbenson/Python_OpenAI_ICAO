38835c84dd173192792b18bf29f26e4c574738b6
Robert Benson <robert.benson@outlook.com>
2024-05-15 17:32:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+client = OpenAI()
+
+load_dotenv()
+my_api_key = os.getenv("OPENAI_API_KEY")
+
+completion = client.chat.completions.create(
+  # model="gpt-3.5-turbo",
+  model="gpt-4o",
+  messages=[
+    {"role": "system", "content": "What ICAO airfields are within 200 km of Dublin airport ?"},
+  ]
+)
+
+print(completion.choices[0].message.content)
